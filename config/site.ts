d3319eb00d3a43c5a80db476835e41bd47fce1dc
Sonu Kumar <sonu.patna0808@gmail.com>
2023-08-08 17:48:58 +0530

    Fix Prettier linter

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
   name: "Memoir",
   description:
     "Unlock the Power of Personal Narratives with Memoir. Discover a dynamic platform where stories come to life. Seamlessly craft, share, and monetize your unique perspectives. Dive into a world of captivating memoirs, insightful articles, and thought-provoking content. Join our community of storytellers and readers today to experience the future of digital narratives. Transform your words into earnings and your memories into meaningful connections. Start your journey with Memoir now.",
-  url: "",
+  url: "https://memoirr.vercel.app",
   ogImage: "",
   links: {
     twitter: "https://twitter.com/sonuk08",
